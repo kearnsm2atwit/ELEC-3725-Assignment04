668a6f5f51c95fbe05aed1b191c79d05a6c4c4a2
kearnsm2atwit <55287446+kearnsm2atwit@users.noreply.github.com>
2020-07-05 22:30:52 -0400

    First commit

---
+++ assignment04.py
@@ -0,0 +1,9 @@
+
+## Need to open and read text files
+
+## https://www.geeksforgeeks.org/reading-writing-text-files-python/
+
+code1 = open("code1.txt", "r")
+
+line1 = code1.readline()
+print(line1)
